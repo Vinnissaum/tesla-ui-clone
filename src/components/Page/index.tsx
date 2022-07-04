9c290eb9fe51526eb1cebe339e41af49a64da462
Vinicius Braz <vinicius_lsb@live.com>
2022-07-03 23:21:46 -0300

    feat: scroll behavior done

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -7,15 +7,27 @@ function Page() {
     <Container>
       <ModelsWrapper>
         <div>
-          <ModelSection
-            modelName="Model One"
-            overlayNode={
-              <DefaultOverlayContent
-                label='Model One'
-                description='Order Online for Delivery'
-              />
-            }
-          />
+          {[
+            'Model One',
+            'Model Two',
+            'Model Three',
+            'Model Four',
+            'Model Five',
+            'Model Six',
+            'Model Seven'
+          ].map((modelName, key) => (
+            <ModelSection
+              key={key}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+          ))}
         </div>
       </ModelsWrapper>
     </Container>
